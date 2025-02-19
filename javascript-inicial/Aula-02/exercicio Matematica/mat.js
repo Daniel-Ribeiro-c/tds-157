008a5c93658ac9d667954e00e2c6ad4abeba0cad
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-19 17:08:01 -0300

    ALTERACAO

--- javascript-inicial/Aula-02/exercicio Matematica/mat.js
+++ javascript-inicial/Aula-02/exercicio Matematica/mat.js
@@ -59,3 +59,53 @@ function media(){
         msg.style.color = "red"
     }
 }
+
+function calcular(){
+    var numero1 = parseFloat(document.getElementById("n9").value)
+    var numero2 = parseFloat(document.getElementById("n10").value)
+    var resulta = document.getElementById("resIMC")
+    var Resultado = numero2 / (numero1 * numero1)
+    var msgt = document.getElementById("msgt")
+    if(Resultado>= 40.0){
+        resulta.textContent = Resultado.toFixed(1)
+        msgt.textContent = "OBESIDADE GRAU 3"
+        msgt.style.color = "red"
+    }else if(Resultado<=39.9 && Resultado>=35.0){
+        resulta.textContent = Resultado.toFixed(1)
+        msgt.textContent = "OBESIDADE GRAU 2"
+        msgt.style.color = "red"
+    }else if(Resultado<=34.9 && Resultado>=30.0){
+        resulta.textContent = Resultado.toFixed(1)
+        msgt.textContent = "OBESIDADE GRAU 1"
+        msgt.style.color = "orange"
+    }else if(Resultado<=29.9 && Resultado>= 25.0){
+        resulta.textContent = Resultado.toFixed(1)
+        msgt.textContent = "SOBREPESO"
+        msgt.style.color = "orange"
+    }else if(Resultado<=24.9 && Resultado>= 18.6){
+        resulta.textContent = Resultado.toFixed(1)
+        msgt.textContent = "NORMAL"
+        msgt.style.color = "green"
+    }else if(Resultado <= 18.5){
+        resulta.textContent = Resultado.toFixed(1)
+        msgt.textContent = "ABAIXO DO NORMAL"
+        msgt.style.color = "blue"
+    }
+    
+}
+function balada(){
+    var numero1 = parseFloat(document.getElementById("n11").value)
+    var resulta = document.getElementById("resBalada")
+    var resultado = 2025 - numero1
+    var msgtd = document.getElementById("msgtd")
+    if(resultado < 18){
+        resulta.textContent = resultado
+        msgtd.textContent = "NÃO PODE ENTRAR XARÁ"
+        msgtd.style.color = "red"
+    }else{
+        resulta.textContent = resultado
+        msgtd.textContent = "Pode entrar lindão"
+        msgtd.style.color = "green"
+    }
+
+}
