b0367d2b6de31124e898f95ab6d9c296d9c878b8
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-19 14:18:59 -0300

    Alteração

--- javascript-inicial/Aula-02/exercicio Matematica/mat.js
+++ javascript-inicial/Aula-02/exercicio Matematica/mat.js
@@ -3,30 +3,37 @@ function somar(){
     var numero2 = parseFloat(document.getElementById("n2").value)
     var res = document.getElementById("res")
     res.innerHTML = numero1 + numero2
-    return res
+   
 }
 
 function subtracao(){
-    var numero3 = parseFloat(document.getElementById("n3").value)
-    var numero4 = parseFloat(document.getElementById("n4").value)
-    var resu = document.getElementById("resu")
-    resu.innerHTML = numero3 - numero4
-    return resu
+    var numero1 = parseFloat(document.getElementById("n3").value)
+    var numero2 = parseFloat(document.getElementById("n4").value)
+    var res = document.getElementById("resu")
+    res.innerHTML = numero1 - numero2
+ 
 }
 
 
 function multiplicacao(){
-    var numero5 = parseFloat(document.getElementById("n5").value)
-    var numero6 = parseFloat(document.getElementById("n6").value)
-    var resul = document.getElementById("resul")
-    resul.innerHTML = numero5 * numero6
-    return resul
+    var numero1 = parseFloat(document.getElementById("n5").value)
+    var numero2 = parseFloat(document.getElementById("n6").value)
+    var res = document.getElementById("resul")
+    res.innerHTML = numero1 * numero2
+
 }
 
 function divisao(){
-    var numero7 = parseFloat(document.getElementById("n7").value)
-    var numero8 = parseFloat(document.getElementById("n8").value)
-    var result = document.getElementById("result")
-    result.innerHTML = numero7 / numero8
-    return result
+    document.getElementById("n8").style.border = "1px solid black"
+    var numero1 = parseFloat(document.getElementById("n7").value)
+    var numero2 = parseFloat(document.getElementById("n8").value)
+    var res = document.getElementById("result")
+    if(numero2 !== 0){
+        res.innerHTML = (numero1 / numero2).toFixed(1)                
+
+    }else{
+        res.innerHTML = "Não é permitido divisão por 0"
+        document.getElementById("n8").style.border = "2px solid red"
+    }
+
 }
